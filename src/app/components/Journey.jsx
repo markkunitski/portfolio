0feb6542a0f8c5b3334ed2181e16b1d70ec38d76
markkunitski <markkmpya@gmail.com>
2023-11-02 17:02:05 +0100

    02.11 0.3v

--- src/app/components/Journey.jsx
+++ src/app/components/Journey.jsx
@@ -2,84 +2,126 @@ import React from "react";
 
 const Journey = () => {
   return (
-    <div>
+    <>
       <div className="elem mb-10">
         <h5 className="mb-2">What sparked my interest in web development?</h5>
         <p>
-          My journey into web development ignited with a burning passion. It all
-          began during my time as a professional esports player, where the quest
-          for greatness became a part of me. Even though my esports journey took
-          an unexpected turn, the desire to excel and create something
-          extraordinary still drives me.
+          I was trying myself as a professional cybersport player, it was my
+          passion for 3 years, but things didn't work out. However, the idea of
+          becoming great at something is still with me. I want to create great
+          things that people will love, and earning their respect is the most
+          satisfying thing for me. I ventured into IT because my best friend has
+          been working in IT for over 10 years, and I believe the best way to
+          learn new things is to have a good teacher. I started with Python and
+          then transitioned to HTML/CSS/JS. Additionally, during my teenage
+          years, I freelanced as a web designer, so it combines my passion and
+          skills.
         </p>
       </div>
+
       <div className="elem mb-10">
-        <h5 className="mb-2">Whats the most inspiring project i worked on?</h5>
+        <h5 className="mb-2">A particular project that stands out</h5>
         <p>
-          The most challenging and rewarding project in my journey has to be
-          NODAUNT. It was a big deal with an even bigger vision: a platform for
-          discovering restaurants and city tours that tailored recommendations
-          based on preferences and location. The main goal was to take the
-          burden of choice off users by offering ready-made orders and tours.
-          Thanks to my web design skills, I took charge of everything, from
-          logic to design.
+          The most remarkable project I've worked on is NODAUNT. It was the most
+          challenging and significant project. The idea behind it was to create
+          a website for finding restaurants and tours in a city based on user
+          preferences and location. The main concept was to alleviate the agony
+          of choice for users by providing them with ready-made orders and
+          tours. People would no longer need assistance to enjoy traveling to
+          new places. I employed the best technologies I know, such as NEXT.js,
+          Tailwind CSS, React, and Leaflet for maps. Given my skills and
+          knowledge in web design, I was responsible for everything the user
+          sees, including the logic and design.
         </p>
       </div>
+
       <div className="elem mb-10">
-        <h5 className="mb-2">What keeps me motivated to work over 80 hours a week?</h5>
+        <h5 className="mb-2">
+          What keeps me motivated during long working hours?
+        </h5>
         <p>
-          I don't mind putting in over 80 hours a week because I believe it's
-          the path to greatness. Inspirations like Elon Musk and David Goggins,
-          who've achieved amazing things, keep me going. While I get that this
-          level of commitment isn't for everyone, I thrive on pushing my limits.
-          Friends worried about burnout only fuel my determination.
+          If you want to become great, you need to increase your chances of
+          success. Working at least twice as hard as others enhances those
+          chances. People like Elon Musk and David Goggins inspire me. I
+          understand that it might seem overwhelming to many, as they worry
+          about the work-life balance. I appreciate that, but I aspire for more
+          and continually strive for it. Burnout is a concern, and many have
+          expressed doubts about my ability to sustain this level of dedication,
+          but their skepticism only fuels my drive. I feel better than ever,
+          especially when I overcome challenges and become smarter, more
+          resilient, and a better developer and person.
         </p>
       </div>
+
       <div className="elem mb-10">
-        <h5 className="mb-2">How did i become proficient in web development?</h5>
+        <h5 className="mb-2">
+          Learning to code and becoming proficient in web development
+        </h5>
         <p>
-          My journey into coding has been nothing short of fascinating. My best
-          friend was my mentor, guiding me from HTML and CSS basics to complex
-          design and logic frameworks. The introduction of chatgpt turbocharged
-          my progress. Beyond the basics, I embarked on a deep dive into
-          computer science, exploring algorithms, data structures, and more,
-          expanding my knowledge exponentially.
+          I began my coding journey with the guidance of a friend who served as
+          my mentor. Starting from scratch, as I had no prior experience, and I
+          struggled with programming in Pascal during my school years. I began
+          with HTML/CSS and gradually introduced JavaScript. I later
+          incorporated frameworks for both design and logic. Fortunately, with
+          the introduction of ChatGPT, my progress accelerated significantly.
+          Beyond the fundamentals, I wanted to understand the core of computer
+          science, so I delved into algorithms and data structures. I completed
+          the popular Berkeley course, CS61A, which was a big hit for me. I also
+          started working with the book "A Programmer's Perspective, 3rd
+          Edition." My knowledge base expanded exponentially, and I gained a
+          comprehensive understanding of how software works.
         </p>
       </div>
+
       <div className="elem mb-10">
-        <h5 className="mb-2">My mentors and key resources in my Journey?</h5>
+        <h5 className="mb-2">
+          Mentors, role models, and influential resources
+        </h5>
         <p>
-          Role models like Elon Musk and David Goggins have a big impact on my
-          aspirations. In the world of programming, ULBI TV on YouTube has been
-          my go-to teacher for frontend and backend development. But the real
-          magic lies in how simple 0s and 1s transform into advanced
-          technologies. I aspire to be one of those innovators driving
-          cutting-edge tech.
+          My friend, who helped me learn to code, has been a valuable mentor and
+          role model. His unwavering support and guidance have been instrumental
+          in my web development journey. I draw inspiration from individuals
+          like Elon Musk and David Goggins, who have achieved remarkable feats
+          through hard work and dedication. While I acknowledge that working 80+
+          hours a week might seem excessive to many, and they worry about
+          work-life balance, my desire for more and my pursuit of excellence
+          keep me motivated. Burnout is a concern, but the doubt expressed by
+          acquaintances only fuels my determination to prove them wrong. I've
+          never felt better, and the most rewarding feeling is the sense of
+          overcoming challenges and becoming a smarter, more resilient, and
+          better individual and developer.
         </p>
       </div>
+
       <div className="elem mb-10">
-        <h5 className="mb-2">How do i stay updated in web development? </h5>
+        <h5 className="mb-2">
+          Keeping up with the evolving field of web development
+        </h5>
         <p>
-          Staying updated in the ever-evolving web development world is crucial,
-          and the internet has fantastic resources. Algorithms tailored for
-          information retrieval ensure I get the knowledge I need, sometimes
-          before I even know I need it. YouTube, in particular, has been a
-          treasure trove of knowledge. Plus, with chatgpt, I can tackle problems
-          with info at my fingertips.
+          Staying updated with industry trends is crucial. I achieve this by
+          reading blogs and articles, attending conferences, and following
+          fellow developers on social media. Active participation in online
+          developer communities and knowledge-sharing platforms is essential.
+          Continuous learning is my mantra to thrive as a developer because the
+          web development field is in a constant state of evolution, and I aim
+          to be at the forefront of these changes.
         </p>
       </div>
-      <div className="elem">
-        <h5 className="mb-2">My future projects and growth plans? </h5>
+
+      <div className="elem mb-10">
+        <h5 className="mb-2">Aspiring projects and continuous growth</h5>
         <p>
-          In the future, I aim to be among the best in the programming world. My
-          journey as a frontend developer is just the beginning of my quest to
-          create software that changes the game. To achieve this, I'm seeking
-          guidance from mentors who can accelerate my progress, letting me push
-          my limits like never before. My commitment to over 80 hours a week is
-          all about my unquenchable thirst for growth and improvement.
+          In the future, I aspire to take on more challenging and complex
+          projects. I'm keen on learning new technologies and expanding my skill
+          set. My growth as a frontend developer will be fueled by reading
+          books, articles, attending conferences, and working on personal
+          projects. Feedback from fellow developers and mentors will also be
+          invaluable on my journey to become the best in the programming field.
+          I'm committed to working 80+ hours a week to achieve my goals and
+          strive for continuous improvement.
         </p>
       </div>
-    </div>
+    </>
   );
 };
 
