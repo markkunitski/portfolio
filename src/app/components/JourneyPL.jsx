3fe4427a8970452dec68fa62e65d0a13af67ca78
markkunitski <markkmpya@gmail.com>
2023-11-02 17:36:53 +0100

    02.11 0.3v

--- src/app/components/JourneyPL.jsx
+++ src/app/components/JourneyPL.jsx
@@ -2,90 +2,134 @@ import React from "react";
 
 const Journey = () => {
   return (
-    <div>
+    <>
       <div className="elem mb-10">
         <h5 className="mb-2">
-          Co wzbudziło moje zainteresowanie rozwojem stron internetowych?
+          Co wywołało moje zainteresowanie rozwojem stron internetowych?
         </h5>
         <p>
-          Moja przygoda z rozwojem stron internetowych rozpoczęła się od
-          płonącej pasji. Wszystko zaczęło się podczas mojej kariery jako
-          zawodowego gracza esportowego, gdzie dążenie do wielkości stało się
-          częścią mnie. Nawet gdy moja przygoda w esporcie przyjęła
-          niespodziewany obrót, pragnienie osiągnięcia doskonałości i tworzenia
-          czegoś niezwykłego nadal mnie napędza.
+          Próbowałem swoich sił jako zawodowy gracz cyber-sportowy, to była moja
+          pasja przez 3 lata, ale niestety nie udało mi się osiągnąć sukcesu.
+          Pomimo tego, idea zostania kimś wielkim nadal ze mną pozostała. Chcę
+          tworzyć wspaniałe rzeczy, które będą kochane przez ludzi, a zdobycie
+          ich szacunku jest dla mnie najbardziej satysfakcjonujące. Wkraczałem w
+          świat IT, ponieważ mój najlepszy przyjaciel pracuje w tej branży od
+          ponad 10 lat, i wierzę, że najlepszym sposobem na naukę nowych rzeczy
+          jest mieć dobrego nauczyciela. Rozpocząłem od Pythona, a potem
+          przeszedłem do HTML/CSS/JS. Dodatkowo, w czasach mojego dorastania,
+          pracowałem jako projektant stron internetowych na zlecenie, co łączyło
+          moją pasję z umiejętnościami.
         </p>
       </div>
+
+      <div className="elem mb-10">
+        <h5 className="mb-2">Szczególny projekt, który wyróżnia się</h5>
+        <p>
+          Najbardziej niezwykłym projektem, nad którym pracowałem, jest NODAUNT.
+          To był najbardziej wymagający i znaczący projekt. Idea polegała na
+          stworzeniu strony internetowej umożliwiającej znalezienie restauracji
+          i wycieczek w danym mieście na podstawie preferencji użytkownika i
+          lokalizacji. Głównym celem było ułatwienie wyboru użytkownikom,
+          dostarczając gotowe zamówienia i wycieczki. Ludzie nie potrzebowaliby
+          już pomocy, aby cieszyć się podróżami do nowych miejsc. Wykorzystałem
+          najlepsze technologie, jakie znam, takie jak NEXT.js, Tailwind CSS,
+          React i Leaflet do map. Dzięki moim umiejętnościom i wiedzy z zakresu
+          projektowania stron internetowych, byłem odpowiedzialny za wszystko,
+          co widzi użytkownik, włączając w to logikę i design.
+        </p>
+      </div>
+
+      <div className="elem mb-10">
+        <h5 className="mb-2">Co mnie motywuje podczas długich godzin pracy?</h5>
+        <p>
+          Jeśli chcesz osiągnąć wielkość, musisz zwiększyć swoje szanse na
+          sukces. Pracowanie co najmniej dwa razy ciężej niż inni zwiększa te
+          szanse. Osoby takie jak Elon Musk i David Goggins mnie inspirują.
+          Rozumiem, że dla wielu może to wydawać się przytłaczające, a obawa o
+          równowagę między pracą a życiem to istotna kwestia. Rozumiem to, ale
+          dążę do więcej i nieustannie to osiągam. Wypalenie zawodowe to
+          rzeczywisty problem, i wielu wyraziło wątpliwości co do mojej
+          zdolności do utrzymania tego poziomu zaangażowania, ale ich sceptycyzm
+          tylko napędza moją determinację. Czuję się lepiej niż kalsycznie,
+          zwłaszcza gdy pokonuję wyzwania i staję się mądrzejszy, bardziej
+          odporny oraz lepszy programista i osoba.
+        </p>
+      </div>
+
       <div className="elem mb-10">
         <h5 className="mb-2">
-          Najbardziej inspirujący projekt, nad którym pracowałem?
+          Nauka kodowania i osiągnięcie biegłości w tworzeniu stron
+          internetowych
         </h5>
         <p>
-          Najbardziej wymagającym i satysfakcjonującym projektem w mojej
-          karierze był NODAUNT. To była duża sprawa z jeszcze większą wizją:
-          platforma do odkrywania restauracji i wycieczek miejskich, która
-          dostosowywała rekomendacje na podstawie preferencji i lokalizacji.
-          Głównym celem było odciążenie użytkowników poprzez oferowanie gotowych
-          zamówień i wycieczek. Dzięki moim umiejętnościom w projektowaniu stron
-          internetowych, objąłem kontrolę nad wszystkim, od logiki po
-          projektowanie.
+          Rozpocząłem swoją przygodę z kodowaniem pod okiem przyjaciela, który
+          był moim mentorem. Rozpoczynając od zera, jako że nie miałem
+          wcześniejszego doświadczenia, i miałem trudności z programowaniem w
+          Pascalu w szkole. Zacząłem od HTML/CSS, a następnie wprowadziłem
+          JavaScript. Później wdrożyłem ramy zarówno do projektowania, jak i
+          logiki. Na szczęście wprowadzenie ChatGPT znacząco przyspieszyło mój
+          postęp. Poza podstawami, chciałem zrozumieć sedno informatyki, dlatego
+          zgłębiałem algorytmy i struktury danych. Ukończyłem popularny kurs na
+          Uniwersytecie Kalifornijskim, CS61A, co było dużym sukcesem dla mnie.
+          Rozpocząłem również pracę z książką "Perspektywa programisty, 3.
+          edycja". Moja baza wiedzy znacząco się poszerzyła, a ja zyskałem
+          kompleksowe zrozumienie działania oprogramowania.
         </p>
       </div>
+
       <div className="elem mb-10">
         <h5 className="mb-2">
-          Co utrzymuje mnie w motywacji do pracy ponad 80 godzin tygodniowo?
+          Mentorzy, wzory do naśladowania i wpływowe źródła
         </h5>
         <p>
-          Nie mam nic przeciwko pracy ponad 80 godzin tygodniowo, ponieważ
-          wierzę, że to droga do wielkości. Inspiracje takie jak Elon Musk i
-          David Goggins, którzy osiągnęli niesamowite rzeczy, napędzają mnie.
-          Chociaż rozumiem, że ten poziom zaangażowania nie jest dla każdego, ja
-          czerpię z niego energię do przekraczania swoich ograniczeń.
-          Przyjaciele martwiący się o wypalenie tylko wzmacniają moją
-          determinację.
+          Mój przyjaciel, który pomógł mi nauczyć się kodować, był cennym
+          mentorem i wzorem do naśladowania. Jego niezłomne wsparcie i
+          przewodnictwo odegrały kluczową rolę w mojej drodze rozwoju w
+          dziedzinie tworzenia stron internetowych. Inspirację czerpię od osób
+          takich jak Elon Musk i David Goggins, którzy osiągnęli niezwykłe
+          sukcesy dzięki ciężkiej pracy i zaangażowaniu. Chociaż rozumiem, że
+          praca 80 godzin tygodniowo może wydawać się zbyt dużym obciążeniem dla
+          wielu i obawiają się o równowagę między pracą a życiem, moje
+          pragnienie osiągnięcia więcej i dążenie do doskonałości sprawiają, że
+          jestem zmotywowany. Wypalenie zawodowe to realny problem, ale
+          wątpliwości wyrażane przez znajomych tylko napędzają moje
+          determinacje, aby udowodnić, że się mylą. Nigdy nie czułem się lepiej,
+          a najbardziej satysfakcjonujące jest pokonywanie wyzwań i stawanie się
+          mądrzejszym, bardziej odpornym oraz lepszym człowiekiem i programistą.
         </p>
       </div>
+
       <div className="elem mb-10">
         <h5 className="mb-2">
-          Jak stałem się biegłym w rozwoju stron internetowych?
+          Krok w krok z ewoluującą dziedziną tworzenia stron internetowych
         </h5>
         <p>
-          Moja przygoda z programowaniem była fascynująca. Mój najlepszy
-          przyjaciel był moim mentorem, prowadząc mnie od podstaw HTML i CSS do
-          skomplikowanych projektów w zakresie projektowania i logiki.
-          Wprowadzenie chatgpt przyspieszyło mój rozwój. Poza podstawami,
-          zagłębiłem się w informatykę, eksplorując algorytmy, struktury danych
-          i wiele innych, co znacząco poszerzyło moją wiedzę.
+          Pozostawanie na bieżąco z trendami w branży jest kluczowe. Osiągam to
+          poprzez czytanie blogów i artykułów, uczestniczenie w konferencjach
+          oraz śledzenie innych programistów na mediach społecznościowych.
+          Aktywny udział w społecznościach deweloperskich online i platformach
+          do dzielenia się wiedzą jest niezbędny. Stałe uczenie się jest moim
+          hasłem, aby odnieść sukces jako programista, ponieważ dziedzina
+          tworzenia stron internetowych jest w ciągłym stanie ewolucji, a ja
+          dążę do bycia na czele tych zmian.
         </p>
       </div>
+
       <div className="elem mb-10">
-        <h5 className="mb-2">
-          Moi mentorzy i kluczowe źródła na mojej drodze?
-        </h5>
+        <h5 className="mb-2">Przyszłe projekty i ciągły rozwój</h5>
         <p>
-          Wzorce do naśladowania takie jak Elon Musk i David Goggins miały duży
-          wpływ na moje aspiracje. W świecie programowania, kanał ULBI TV na
-          YouTube był moim nauczycielem do spraw rozwoju frontendu i backendu.
-          Ale prawdziwa magia polega na tym, jak proste zera i jedynki
-          przekształcają się w zaawansowane technologie. Aspiruję do bycia
-          jednym z tych innowatorów, którzy napędzają najnowocześniejszą
-          technologię.
+          W przyszłości pragnę podjąć się bardziej wymagających i
+          skomplikowanych projektów. Chętnie uczę się nowych technologii i
+          poszerzam swoje umiejętności. Mój rozwój jako programisty front-end
+          będzie napędzany czytaniem książek, artykułów, uczestnictwem w
+          konferencjach i pracą nad projektami osobistymi. Opinie innych
+          programistów i mentorów będą również bezcenne w mojej drodze do
+          zostania najlepszym w dziedzinie programowania. Jestem zobowiązany do
+          pracy 80+ godzin tygodniowo, aby osiągnąć swoje cele i dążyć do
+          ciągłego doskonalenia.
         </p>
       </div>
-      <div className="elem">
-        <h5 className="mb-2">
-          Jak pozostaję na bieżąco w rozwoju stron internetowych?
-        </h5>
-        <p>
-          Pozostanie na bieżąco w świecie ciągle ewoluującym rozwoju stron
-          internetowych jest kluczowe, a internet oferuje fantastyczne zasoby.
-          Algorytmy dostosowane do pozyskiwania informacji zapewniają mi wiedzę,
-          czasem nawet zanim jeszcze wiem, że jej potrzebuję. YouTube, w
-          szczególności, stanowi skarbnicę wiedzy. Dodatkowo, dzięki chatgpt,
-          mogę rozwiązywać problemy z informacjami na wyciągnięcie ręki.
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
 
